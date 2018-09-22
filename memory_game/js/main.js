1a9e675f6f9eff9788e79b12ab2801b0b6aac7b6
ABDULRAHMAN ALHDLAK <abdulrahmanalhdlak@ABDULRAHMANs-MacBook-Air.local>
2018-09-22 19:54:54 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -10,7 +10,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+}
+
+var flipCard = function(cardId){
+	checkForMatch();
+	console.log("User flipped"+ " " + cards[cardId]) ;
+	cardsInPlay.push(cards[cardId]);
+}
+flipCard(0);	
+
+flipCard(2);	
+
+/*var cardOne = cards[0];
 cardsInPlay.push(cardOne);
 console.log("User flipped queen");
 
@@ -26,4 +43,5 @@ alert("You found a match!");
 	else{
 		alert("Sorry, try again.");
 	}
-}
+
+}*/
